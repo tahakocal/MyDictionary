2cee6cabb9a9a37a03565e7f73599229b66b6edc
tahakocal <tahakocalgs@gmail.com>
2021-04-25 19:18:54 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T, V>
+    {
+        T[] keys;
+        V[] values;
+
+        public MyDictionary()
+        {
+            keys = new T[0];
+            values = new V[0];
+        }
+
+        public void Add(T Keys, V Values)
+        {
+            T[] tempKeys = keys;
+            V[] tempValues = values;
+
+            keys = new T[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = Keys;
+            values[values.Length - 1] = Values;
+
+
+
+        }
+
+        public int keysLength
+        {
+            get { return keys.Length; }
+        }
+
+        public int valuesLength
+        {
+            get { return values.Length; }
+        }
+
+
+
+        public T[] Keys
+        {
+            get { return keys; }
+        }
+
+        public V[] Values
+        {
+            get { return values; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+            myDictionary.Add("Istanbul", 34);
+            myDictionary.Add("Ankara", 06);
+            myDictionary.Add("Adana", 01);
+
+            Console.WriteLine(myDictionary.keysLength);
+            Console.WriteLine(myDictionary.valuesLength);
+
+
+            Console.ReadLine();
+        }
+    }
+}
